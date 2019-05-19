6c52cd57a9e0fe76a284f60a8a005f034a9f2416
zhouchenhua <noatnu@163.com>
2019-05-19 17:09:19 +0800

    4346643

--- other/src/main/java/regex/RegexDemo.java
+++ other/src/main/java/regex/RegexDemo.java
@@ -1,6 +1,5 @@
 package regex;
 
-import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.Test;
 import tool.utils.FileUtils;
@@ -8,6 +7,7 @@ import tool.utils.FileUtils;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -19,14 +19,21 @@ import java.util.stream.Collectors;
  **/
 public class RegexDemo {
 
-
+    private Logger logger = Logger.getLogger(this.toString());
     /**
      * 基础匹配
      */
     @Test
     public void testA() {
-        Pattern pattern = Pattern.compile("to");
-        Matcher matcher = pattern.matcher("Customers like to grab a chance to pick up a doll");
+        //这里使用的是正则表达式是纯文本，它将匹配原始文本里的 Sunday
+        Pattern pattern = Pattern.compile("Sunday");
+        Matcher matcher = pattern.matcher("It was Sunday. I never get up early on Sundays. I sometimes stay in bed until lunch time. Last Sunday I got up very late. ");
+        if (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+        //匹配一个字符
+        pattern = Pattern.compile("最");
+        matcher = pattern.matcher("要么做第一个，要么做最好的一个");
         if (matcher.find()) {
             System.out.println(matcher.group());
         }
@@ -37,24 +44,19 @@ public class RegexDemo {
      */
     @Test
     public void testPoint() {
+        //在正则表达式里，特殊字符（或字符集和）用来给出要搜索的东西。 .字符可以匹配任何一个单个的字符
         Pattern pattern = Pattern.compile("Cannes.");
         Matcher matcher = pattern.matcher("Celebrities shine at the opening of Cannes。");
         if (matcher.find()) {
             System.out.println(matcher.group());
         }
+        List<String> stringList = Arrays.asList(new File(String.format("%shelp",FileUtils.getMainDataDir(this.getClass(), ""))).listFiles()).stream().map(File::getName).peek(s -> System.out.println()).collect(Collectors.toList());
+        String string =  "This interface contains query methods used to determine" ;
+        pattern = Pattern.compile("in.");
+        matcher = pattern.matcher(string);
+        while (matcher.find()) System.out.println(matcher.group());
     }
 
-    /***
-     * 匹配一个字符
-     */
-    @Test
-    public void firstMatchWord() {
-        Pattern pattern = Pattern.compile("最");
-        Matcher matcher = pattern.matcher("要么做第一个，要么做最好的一个");
-        if (matcher.find()) {
-            System.out.println(matcher.group());
-        }
-    }
 
     /**
      * 转义
@@ -62,11 +64,14 @@ public class RegexDemo {
     @Test
     public void testEscape() {
         List<String> stringList = Arrays.asList(new File(FileUtils.getMainDataDir(this.getClass(), "")).listFiles()).stream().map(File::getName).peek(System.out::println).collect(Collectors.toList());
-        Pattern pattern = Pattern.compile(String.format("%s%s", this.getClass().getSimpleName(), "\\.java"));
-        Matcher matcher = pattern.matcher(StringUtils.join(stringList, ","));
-        if (matcher.find()) {
-            System.out.println("====" + matcher.group());
-        }
+        handle(String.format("%s%s", this.getClass().getSimpleName(), "\\.java"),StringUtils.join(stringList, ","));
+    }
+
+
+    /*匹配单个字符*/
+    @Test
+    public void firstMatcher(){
+        handle(".a.","na1 na2 sa1");
     }
 
     /*匹配一组字符*/
@@ -85,5 +90,28 @@ public class RegexDemo {
         while (m.find()) System.out.println(m.group());
     }
 
+    //组 匹配
+    @Test
+    public void testGroupRange(){
+        handle("[ns]a.\\.xls","sales1.xls orders3.xls sales2.xls na1.xls na2.xls ca1.xls");
+    }
+
+    @Test
+    public void sidingToSidingBlockNumber(){
+        handle("[0-9][0-9][0-9][0-9]","Copyright 1995 - 2019 . All rights reserved. The content (including but not limited to text, photo, multimedia information, etc) published in this site belongs to China Daily ");
+    }
+
+    private void handle(String regex,String input){
+        //获得调用者的方法名
+        String _methodName = new Exception().getStackTrace()[1].getMethodName();
+        //获得当前的方法名
+        String _thisMethodName = new Exception().getStackTrace()[0].getMethodName();
+        System.out.println(_thisMethodName);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            System.out.println(_methodName+":"+matcher.group());
+        }
+    }
 
 }
--- other/src/main/java/regex/readme.md
+++ other/src/main/java/regex/readme.md
@@ -1,1 +1,20 @@
 #Java正则表达式
+> 正则表达式是一种可以用于模式匹配和替换的强有力的工具
+
++ 基础匹配
+```
+Pattern pattern = Pattern.compile("to");
+Matcher matcher = pattern.matcher("Customers like to grab a chance to pick up a doll");
+if (matcher.find()) {
+    System.out.println(matcher.group());
+}
+```
++ .可以匹配任何字符
+```
+//在正则表达式里，特殊字符（或字符集和）用来给出要搜索的东西。 .字符可以匹配任何一个单个的字符
+Pattern pattern = Pattern.compile("Cannes.");
+Matcher matcher = pattern.matcher("Celebrities shine at the opening of Cannes。");
+if (matcher.find()) {
+    System.out.println(matcher.group());
+}
+```
